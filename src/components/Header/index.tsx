5ddf81d119a22f424df1ab58817e3bf22370ff28
Gabriel Saliba <gabriel.saliba.179@gmail.com>
2021-04-21 11:42:50 -0300

    implementando header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
+
+import styles from './styles.module.scss'
+
+export function Header() {
+  //https://date-fns.org/v2.21.1/docs/format
+  const currentDate = format(new Date(), 'EEEEEEE, d MMM', {
+    locale: ptBR,
+  })  
+
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr"/>
+
+      <p>O melhor para você ouvir, sempre</p>
+
+      <span>{currentDate}</span>
+    </header>
+  );
+}
