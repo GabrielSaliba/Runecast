42c8e48ceec199541b5baaaec031521af04e1614
GabrielSaliba <gabriel.saliba.179@gmail.com>
2021-05-24 10:43:28 -0300

    server

---
+++ server/server.ts
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('server.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3333;
+
+console.log('Server running in port: ', port)
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
